b3a78c7d7a6c56d840881899c757b7e965a0bd47
Edwin GC <edwingalcol@gmail.com>
2025-07-01 21:35:28 -0600

    V5_1

--- src/main/java/com/example/sistemaweb/sistemaweb/Controller/AlumnoController.java
+++ src/main/java/com/example/sistemaweb/sistemaweb/Controller/AlumnoController.java
@@ -7,10 +7,13 @@ import com.example.sistemaweb.sistemaweb.Services.*;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -55,42 +58,24 @@ public class AlumnoController {
     }
     */
 
-    @GetMapping("/alumno_grupo")
-    public String alumno_grupo1(@RequestParam("file") MultipartFile file, 
-    @RequestParam("idGrupo") Long idGrupo, 
-    @RequestParam("idMateria") Long idMateria, Model model) {
-        try {
-            Grupo grupo = grupoRepository.findById(idGrupo).orElseThrow(() -> new Exception("Grupo no encontrado"));
-            Materia materia = materiaRepository.findById(idMateria).orElseThrow(() -> new Exception("Materia no encontrada"));
-            calificacionesService.cargarAlumnosDesdeExcelCompletoREs(file, grupo, materia);
-            //alumnoService.cargarAlumnosDesdeExcel(file, grupo);
-            model.addAttribute("success", "Alumnos cargados y asignados al grupo correctamente.");
-        } catch (Exception e) {
-            e.printStackTrace();
-            model.addAttribute("error", "Hubo un error al cargar el archivo: " + e.getMessage());
-        }
-        return "/Proyecto/cargarCalificaciones"; // Regresa a la misma vista
-    }
     
 
-
+    @ResponseBody
     @PostMapping("/alumno_grupo")
-    public String alumno_grupo(@RequestParam("file") MultipartFile file, 
-    @RequestParam("idGrupo") Long idGrupo, 
-    @RequestParam("idMateria") Long idMateria, Model model) {
+    public ResponseEntity<String> alumno_grupo(@RequestParam("file") MultipartFile file, 
+                                            @RequestParam("idGrupo") Long idGrupo, 
+                                            @RequestParam("idMateria") Long idMateria) {
         try {
             Grupo grupo = grupoRepository.findById(idGrupo).orElseThrow(() -> new Exception("Grupo no encontrado"));
             Materia materia = materiaRepository.findById(idMateria).orElseThrow(() -> new Exception("Materia no encontrada"));
             calificacionesService.cargarAlumnosDesdeExcelCompletoREs(file, grupo, materia);
-            //alumnoService.cargarAlumnosDesdeExcel(file, grupo);
-            model.addAttribute("success", "Alumnos cargados y asignados al grupo correctamente.");
+            return ResponseEntity.ok("OK");
         } catch (Exception e) {
             e.printStackTrace();
-            model.addAttribute("error", "Hubo un error al cargar el archivo: " + e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error: " + e.getMessage());
         }
-        return "/Proyecto/cargarCalificaciones"; // Regresa a la misma vista
     }
-    
+
     
 
 
